529c05b8827481772c7070c22bf875a7714312cf
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2023-07-14 00:25:55 +0300

    replaced JSON.parse with jsonc-parser to support commented json files

--- src/loaders/jsonLoader.ts
+++ src/loaders/jsonLoader.ts
@@ -1,5 +1,6 @@
+import { parse } from 'jsonc-parser';
 import Loader from '../utils/Loader';
 
-const jsonLoader = new Loader('json', JSON.parse);
+const jsonLoader = new Loader('json', (input) => parse(input));
 
 export default jsonLoader;
